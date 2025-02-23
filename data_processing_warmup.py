9e2c7e33af65c0a647f60095e5c0106132e4b77c
Ahmad Yusuf Albadri <ayusufalba25@gmail.com>
2025-02-23 09:00:09 +0700

    Update

--- data_processing_warmup.py
+++ data_processing_warmup.py
@@ -1,2 +1,9 @@
 def preprocessing_warmup(data):
-    
+    remove_column = [
+        'Lahan Hutan',
+        'Emisi Total',
+        'Populasi Pedesaan',
+        'Populasi Perkotaan',
+        
+    ]
+    # Get total population = men total population + female total population
